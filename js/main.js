aebecd0cb831498981af6b2b015295107da7a407
kostia <galion-trade@tut.by>
2020-08-01 21:20:00 +0300

    create object personalMovieDB

--- js/main.js
+++ js/main.js
@@ -2,3 +2,12 @@
 
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","");
 
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
